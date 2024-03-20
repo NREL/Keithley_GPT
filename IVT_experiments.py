296cb1455f46f008c308f8edad7c09841a919e4f
Febba, Davi Marcelo <DaviMarcelo.Febba@nrel.gov>
2024-03-20 09:47:37 -0600

    gas_data

--- IVT_experiments.py
+++ IVT_experiments.py
@@ -1,39 +1,12 @@
-import McAllister
 from Keithley2400GPT import Keithley2400Controller
 from matplotlib import pyplot as plt
 import numpy as np
-from matplotlib import pyplot as plt
 import os
-
-# Create instrument instances
-# mcallister = McAllister(host='169.254.160.136', port=502)
-# keithley = Keithley2400Controller(timeout=25000)
-
-# # Instrument setup
-# mcallister.connect()
-# mcallister.enable_remote_operation(enable=True)
-# mcallister.enable_master(enable=True)
 
 keithley = Keithley2400Controller(timeout=50000)
 keithley.connect()
 keithley.select_panel('FRONT')
 keithley.set_measurement_mode(2)
-
-# setpoint = 100.0  # Target temperature
-# reach_result = mcallister.check_temperature_reach(setpoint)
-# if reach_result:
-#     stability_result, stable_temp = mcallister.check_temperature_stability(setpoint)
-#     if stability_result:
-#         print(f"Temperature stabilized at {stable_temp}.")
-#     else:
-#         print("Failed to stabilize temperature within timeout.")
-# else:
-#     print("Proceeding to stability check despite not reaching setpoint within timeout.")
-#     stability_result, stable_temp = mcallister.check_temperature_stability(setpoint)
-#     if stability_result:
-#         print(f"Temperature stabilized at {stable_temp}.")
-#     else:
-#         print("Failed to stabilize temperature.")
 
 # IV sweep
 voltage, current = keithley.iv_sweep(source_type='VOLT', measure_type='CURR', 
@@ -53,9 +26,3 @@ os.makedirs(iv_folder, exist_ok=True)
 with open(os.path.join(iv_folder,file_path), 'w') as file:
     for s, m in zip(voltage, current):
         file.write(f"{s}\t{m}\n")
-
-
-# Disable remote operation and power and close the connection
-#mcallister.enable_master(enable=False)
-#mcallister.enable_remote_operation(enable=False)
-#mcallister.close()
